7db945dcf435266378392c78aadc93a2d16b0c57
Bryan <francotte.bryan@gmail.com>
2018-01-05 13:26:24 +0100

    Notification update

--- CoursierWallonBackOffice/Model/ApplicationUser.cs
+++ CoursierWallonBackOffice/Model/ApplicationUser.cs
@@ -10,23 +10,12 @@ namespace CoursierWallonBackOffice.Model
     public class ApplicationUser
     {
         public string Id { get; set; }
-        public long? AddressIdUser { get; set; }
-        public DateTime? BirthDate { get; set; }
         public byte[] VerCol { get; set; }
-        public Address AddressIdUserNavigation { get; set; }
         [Timestamp]
         public byte[] RowVersion { get; set; }
-        public int AccessFailedCount { get; set; }
-        public string ConcurrencyStamp { get; set; }
-        public bool EmailConfirmed { get; set; }
-        public bool LockoutEnabled { get; set; }
-        public DateTimeOffset? LockoutEnd { get; set; }
-        public string NormalizedEmail { get; set; }
-        public string NormalizedUserName { get; set; }
-        public string PasswordHash { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool PhoneNumberConfirmed { get; set; }
-        public string SecurityStamp { get; set; }
-        public bool TwoFactorEnabled { get; set; }
+       
+        public string UserName { get; set; }
+
+        public string AndroidToken { get; set; }
     }
 }
